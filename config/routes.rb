cb1855bffd038b61b9c6cd2c2c9c918a5b7851d6
Sean Graham <seanmgrahamnj@yahoo.com>
2016-09-13 20:41:57 -0400

    added Devise and worked out basic login/logout and home, slice, businesses hierarchy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
+    resources :searches do
+      resources :businesses
+    end
+
+
+
   root 'welcome#index'
+
 end
